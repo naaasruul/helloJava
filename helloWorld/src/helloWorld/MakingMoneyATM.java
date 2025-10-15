9dab5b4e45234de75c4676501843a0a6e8fcc570
USER <USER@NasEduLappy>
2025-10-15 09:47:22 +0800

    sajdskad

--- helloWorld/src/helloWorld/MakingMoneyATM.java
+++ helloWorld/src/helloWorld/MakingMoneyATM.java
@@ -8,12 +8,6 @@ public class MakingMoneyATM {
 		System.out.println("*                                                                  *");
 		System.out.println("********************************************************************");
 		System.out.println("");
-		System.out.println("Welcome!");
-		
-		for(int i=1; i<10; i++) {
-			if(i % 2 == 0) {
-				System.out.println(i);
-			}
-		}
+		System.out.println("Welcome!");	
 	}
 }
